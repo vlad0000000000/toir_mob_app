ceef6a36ade7bfcdf19892b3bb314f6d9f72131a
Vlad Miheev <miheev.vlad@gmail.com>
2025-06-21 14:28:37 +0300

    upd demo name

--- lib/main.dart
+++ lib/main.dart
@@ -172,7 +172,7 @@ class MyApp extends StatelessWidget {
 
               var app = MaterialApp.router(
                 builder: EasyLoading.init(),
-                title: 'QR Machine Scanner',
+                title: 'QR Machine Scanner Demo',
                 theme: ThemeData.from(
                   colorScheme: ColorScheme.fromSeed(
                       seedColor: Colors.blue,
